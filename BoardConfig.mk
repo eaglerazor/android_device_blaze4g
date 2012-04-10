2650f16c2d7ddb7a50f6a73a4c94c731d197cb12
David Rouleau <Shabbypenguin@gmail.com>
2012-04-10 00:24:59 -0400

    working cwm

---
+++ BoardConfig.mk
@@ -0,0 +1,111 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+# inherit from the proprietary version
+-include vendor/samsung/blaze4g/BoardConfigVendor.mk
+
+USE_CAMERA_STUB := true
+BOARD_USES_GENERIC_AUDIO := false
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RADIOIMAGE := true
+
+TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
+TARGET_BOARD_PLATFORM := msm8660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := neon
+TARGET_CPU_SMP := true
+
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+
+BOARD_USE_CUSTOM_RECOVERY_FONT := "\"roboto_15x24.h\""
+TARGET_RECOVERY_INITRC := device/samsung/blaze4g/recovery/init.rc
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/blaze4g/recovery/graphics.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/blaze4g/recovery/recovery_keys.c
+BOARD_CUSTOM_RECOVERY_UI := ../../device/samsung/blaze4g/recovery/recovery_ui.c
+BOARD_HAS_SDCARD_INTERNAL := true
+BOARD_HAS_INTERNAL_PARTITIONS := true
+
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_watchdog.appsbark=0 msm_watchdog.enable=1 loglevel=4
+BOARD_KERNEL_BASE := 0x40400000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_FORCE_RAMDISK_ADDRESS := 0x41900000
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00a00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00a00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x25600000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x80200000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/samsung/blaze4g/kernel
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_USES_MMCUTILS := true
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_MAX_PARTITIONS := 28
+BOARD_USES_SAMSUNG_AUDIO_STUB := true
+
+BOARD_DATA_DEVICE := /dev/block/mmcblk0p25
+BOARD_DATA_FILESYSTEM := ext4
+BOARD_DATA_FILESYSTEM_OPTIONS := nosuid,nodev,relatime,barrier=1,noauto_da_alloc
+BOARD_SYSTEM_DEVICE := /dev/block/mmcblk0p24
+BOARD_SYSTEM_FILESYSTEM_OPTIONS := noatime,nodiratime,barrier=1,noauto_da_alloc
+BOARD_SYSTEM_FILESYSTEM := ext4
+BOARD_CACHE_DEVICE := /dev/block/mmcblk0p26
+BOARD_CACHE_FILESYSTEM := ext4
+BOARD_CACHE_FILESYSTEM_OPTIONS := nosuid,nodev,relatime,barrier=1,noauto_da_alloc
+
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+#BOARD_SDCARD_DEVICE_INTERNAL := /dev/block/mmcblk0p28
+BOARD_UMS_LUNFILE := /sys/devices/platform/usb_mass_storage/lun1/file
+
+USE_OPENGL_RENDERER :=true
+
+# WiFi
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION      := VER_0_6_X
+BOARD_WLAN_DEVICE           := bcm4330
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_STA_PATH     := "/system/etc/wifi/bcm4330_sta.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/system/etc/wifi/bcm4330_aps.bin"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcm4330_mfg.bin nvram_path=/system/etc/wifi/nvram_mfg.txt"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
---
+++ device_blaze4g.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/samsung/blaze4g/blaze4g-vendor.mk)
+
+# Set en_US as default locale
+PRODUCT_LOCALES := en_US
+
+# blaze4g uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ueventd.rc:root/ueventd.rc \
+    $(LOCAL_PATH)/init.qcom.lpm_boot.sh:root/init.qcom.lpm_boot.sh \
+    $(LOCAL_PATH)/init.qcom.sh:root/init.qcom.sh \
+    $(LOCAL_PATH)/lpm.rc:root/lpm.rc \
+    $(LOCAL_PATH)/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/samsung/blaze4g/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/samsung/blaze4g/overlay
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+PRODUCT_NAME := full_blaze4g
+PRODUCT_DEVICE := blaze4g
+PRODUCT_MODEL := SGH-T789
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_BRAND := generic
